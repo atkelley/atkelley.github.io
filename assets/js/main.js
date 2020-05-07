e602201ab5ea9ad24e29e0ef382e38202ee37118
Andrew T. Kelley <kelley.andrew.t@gmail.com>
2020-05-07 13:47:18 -0400

    Improved #contact click to land at right place for Firefox

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,28 @@
 function setScrollingEvent(){
   $('#intro').click(function(){
-      $('#intro-container').animatescroll({padding: 200});
+    $('#intro-container').animatescroll({padding: 200});
   });
 
   $('#about').click(function(){
-      $('#about-container').animatescroll({padding: 150});
+    $('#about-container').animatescroll({padding: 150});
   });
 
   $('#skills').click(function(){
-      $('#skills-container').animatescroll({padding: 100});
+    $('#skills-container').animatescroll({padding: 100});
   });
 
   $('#portfolio').click(function(){
-      $('#portfolio-container').animatescroll({padding: 100});
+    $('#portfolio-container').animatescroll({padding: 100});
   })
 
   $('#contact').click(function(){
+    if(navigator.userAgent.search(/webkit/i)>0) { // Chrome, Safari
       $('#contact-container').animatescroll({padding: 25});
+    } else if(navigator.userAgent.search(/gecko/i)>0) { // Firefox
+      $('#contact-container').animatescroll({padding: 0});
+    } else if(navigator.userAgent.search(/???/i)>0){  // Others
+      $('#contact-container').animatescroll({padding:0});
+    } 
   });
 }   
 
